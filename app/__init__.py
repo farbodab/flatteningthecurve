c65985039a06ad85929fe999a050499a3068a543
unknown <farbod.ab@hotmail.ca>
2020-03-24 18:05:32 -0400

    added cors

--- app/__init__.py
+++ app/__init__.py
@@ -15,6 +15,7 @@ def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
 
+    cors.init_app(app)
     db.init_app(app)
     migrate.init_app(app, db)
     flaskjson.init_app(app)
