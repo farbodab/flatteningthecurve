211714ae971fe6b26f1208a1da9189201b773a5a
unknown <farbod.ab@hotmail.ca>
2020-03-28 13:11:47 -0400

    added background refresher

--- server.py
+++ server.py
@@ -4,7 +4,17 @@ import click
 from app import create_app, db
 from app.models import *
 from flask_migrate import Migrate
+from apscheduler.schedulers.background import BackgroundScheduler
+from app.data.routes import update
+
+def sensor():
+    with app.app_context():
+        update()
+        print('refreshed')
 
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 migrate = Migrate(app, db)
+sched = BackgroundScheduler(daemon=True)
+sched.add_job(sensor,'interval',minutes=180)
+sched.start()
