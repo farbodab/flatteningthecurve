dfdc335598c752fdb3cecebbdf646d148f09b31b
James Hooks <james.hooks@mail.utoronto.ca>
2020-04-17 14:42:33 -0400

    Government response and npi interventions - usa datasets

--- app/export/sheetsHelper.py
+++ app/export/sheetsHelper.py
@@ -15,22 +15,24 @@ import json
 scopes = ['https://www.googleapis.com/auth/spreadsheets', "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
 
 collections = [
-    {'name':'Results','data':api.vis.get_results},
-    {'name':'PHU','data':api.vis.get_phus},
-    {'name':'Growth','data':api.vis.get_growth},
-    {'name':'Growth_Recent','data':api.vis.get_growth_recent},
-    {'name':'Test Results','data':api.vis.get_testresults},
-    {'name':'ICU Capacity','data':api.vis.get_icu_capacity},
-    {'name':'ICU Capacity Province','data':api.vis.get_icu_capacity_province},
-    {'name':'ICU Case Status Province','data':api.vis.get_icu_case_status_province},
-    {'name':'Canada Mobility','data':api.vis.get_mobility},
-    {'name':'Canada Mobility Transportation','data':api.vis.get_mobility_transportation},
-    {'name':'Canada Testing','data':api.vis.get_tested},
-    {'name':'Canada Deaths','data':api.vis.get_deaths},
-    {'name':'Average Daily Cases (7-day rolling)','data':api.vis.get_cases_rolling_average},
-    {'name':'Average Daily Deaths (7-day rolling)','data':api.vis.get_deaths_rolling_average},
-    {'name':'Daily Deaths','data':api.vis.get_daily_deaths},
-    {'name':'Top Causes','data':api.vis.get_top_causes}
+    {'name':'Results','function':api.vis.get_results},
+    {'name':'PHU','function':api.vis.get_phus},
+    {'name':'Growth','function':api.vis.get_growth},
+    {'name':'Growth_Recent','function':api.vis.get_growth_recent},
+    {'name':'Test Results','function':api.vis.get_testresults},
+    {'name':'ICU Capacity','function':api.vis.get_icu_capacity},
+    {'name':'ICU Capacity Province','function':api.vis.get_icu_capacity_province},
+    {'name':'ICU Case Status Province','function':api.vis.get_icu_case_status_province},
+    {'name':'Canada Mobility','function':api.vis.get_mobility},
+    {'name':'Canada Mobility Transportation','function':api.vis.get_mobility_transportation},
+    {'name':'Canada Testing','function':api.vis.get_tested},
+    {'name':'Canada Deaths','function':api.vis.get_deaths},
+    {'name':'Average Daily Cases (7-day rolling)','function':api.vis.get_cases_rolling_average},
+    {'name':'Average Daily Deaths (7-day rolling)','function':api.vis.get_deaths_rolling_average},
+    {'name':'Daily Deaths','function':api.vis.get_daily_deaths},
+    {'name':'Top Causes','function':api.vis.get_top_causes},
+    {'name':'Government Response','table':'governmentresponse'},
+    {'name':'NPI Interventions - USA','table':'npiinterventions_usa'}
 ]
 
 def parseVal(val):
@@ -49,7 +51,11 @@ def getSheet(sheetName, sh, rows, cols):
 
 def updateCollection(dataSource, sh):
     try:
-        df = dataSource['data']()
+        df = None
+        if 'function' in dataSource:
+            df = dataSource['function']()
+        elif 'table' in dataSource:
+            df = pd.read_sql_table(dataSource['table'], db.engine)
         sheet = getSheet(dataSource['name'], sh, df.shape[0], df.shape[1])
         print("Update collection", dataSource['name'])
         set_with_dataframe(sheet, df, row=1, col=1, include_index=False, include_column_header=True, resize=True, allow_formulas=True)
