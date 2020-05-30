2e107dcdb825be11e2370081cb73eca06d116225
unknown <farbod.ab@hotmail.ca>
2020-05-30 11:07:01 -0400

    moved order

--- app/data/__init__.py
+++ app/data/__init__.py
@@ -115,7 +115,6 @@ PHU = ['the_district_of_algoma',
 
 @bp.cli.command('ontario')
 def getontario():
-    routes.confirmed_ontario()
     routes.testsnew()
     routes.getlongtermcare()
     routes.getpredictivemodel()
