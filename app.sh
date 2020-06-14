94a73c9113966f48c381445cd7e751d5c6be5add
unknown <farbod.ab@hotmail.ca>
2020-06-13 21:36:20 -0400

    attempt 2

--- app.sh
+++ app.sh
@@ -1,21 +1,2 @@
-# Versions
-CHROME_DRIVER_VERSION=`curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE`
-
-# Install dependencies.
-sudo yum update
-sudo yum install -y unzip openjdk-8-jre-headless xvfb libxi6 libgconf-2-4
-
-# Install Chrome.
-vi /etc/yum.repos.d/google-chrome.repo
-
-yum install -y google-chrome-stable
-
-# Install ChromeDriver.
-wget -N https://chromedriver.storage.googleapis.com/2.35/chromedriver_linux64.zip -P ~/
-unzip ~/chromedriver_linux64.zip -d ~/
-rm ~/chromedriver_linux64.zip
-sudo mv -f ~/chromedriver /usr/local/bin/chromedriver
-sudo chown root:root /usr/local/bin/chromedriver
-sudo chmod 0755 /usr/local/bin/chromedriver
 
 gunicorn -b 0.0.0.0:8080 --workers=12 server:app
