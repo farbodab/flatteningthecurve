44855bd44b5f796dabbcb5fee59c420e28725a4b
unknown <farbod.ab@hotmail.ca>
2020-06-15 09:56:33 -0400

    fixed

---
+++ staging/versions/91a4b71ec6bb_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 91a4b71ec6bb
+Revises: 4240abc4d175, d35cfdf225a0
+Create Date: 2020-06-15 09:48:54.871114
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '91a4b71ec6bb'
+down_revision = ('4240abc4d175', 'd35cfdf225a0')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
